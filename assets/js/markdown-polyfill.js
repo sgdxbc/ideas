8e6acb1fab327a30e5874842d36457cc568745ae
sgdxbc <sgdxbc@gmail.com>
2023-11-16 11:59:52 +0000

    Deploying to gh-pages from @ sgdxbc/ideas@11bdcf82e07a9ef3b67e4e6ee668f2968684f0a5 🚀

--- assets/js/markdown-polyfill.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    for (let node of document.querySelectorAll('pre')) {
-        node.classList.add('highlight');
-        const wrapped = document.createElement('div');
-        wrapped.classList.add('highlight');
-        node.parentElement.insertBefore(wrapped, node);
-        wrapped.appendChild(node);
-    }
-});
